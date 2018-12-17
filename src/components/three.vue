1675b228585be3cd6289c2094fa0c78462528491
yzhenxs <738534081@qq.com>
2018-12-17 22:42:17 +0800

    第一次

---
+++ src/components/three.vue
@@ -0,0 +1,32 @@
+<template>
+ <div class="three" :style="{backgroundColor:beijing}">第三个路由
+     <br>
+   <button @click="bianse">点我变色</button>
+   <Sun></Sun>
+ </div>
+</template>
+
+<script>
+import Sun from "./sun"
+export default {
+  name: 'three',
+  components:{
+    Sun
+  },
+  data () {
+    return {
+        beijing:"blue"
+        // msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods:{
+    bianse(){
+     this.beijing='greenyelow'
+    }
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
---
+++ src/components/two.vue
@@ -0,0 +1,32 @@
+<template>
+ <div class="two" :style="{backgroundColor:beijing}">第二个路由
+     <br>
+   <button @click="bianse">点我变色</button>
+   <Sun></Sun>
+ </div>
+</template>
+
+<script>
+import Sun from "./sun"
+export default {
+  name: 'two',
+  components:{
+    Sun
+  },
+  data () {
+    return {
+        beijing:"green"
+        // msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods:{
+    bianse(){
+     this.beijing='yellow'
+    }
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
